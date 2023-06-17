d79f77c016f388e1cf73d42016ab0c464f3e7046
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-06-17 19:57:09 +0530

    Move Zeros to end optimal

--- Arrays-Easy/MoveZerosToEnd.playground/Contents.swift
+++ Arrays-Easy/MoveZerosToEnd.playground/Contents.swift
@@ -6,7 +6,7 @@ import UIKit
  Problem Statement: You are given an array of integers, your task is to move all the zeros in the array to the end of the array and move non-negative integers to the front by maintaining their order.
  Examples:
  Example 1:
- Input: 1 ,0 ,2 ,3 ,0 ,4 ,0 ,1
+ Input: 1 ,0 ,0,2 ,3 ,0 ,4 ,0 ,1
  Output: 1 ,2 ,3 ,4 ,1 ,0 ,0 ,0
  Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
 
@@ -16,19 +16,50 @@ import UIKit
  Explanation: All the zeros are moved to the end and non-negative integers are moved to front by maintaining order
  */
 
+func moveAllZerosToEndOPTIMAL(_ a:[Int]) -> [Int] {
+    var j = 0
+    var A = a
+    
+    for i in 0..<a.count {
+        if A[i] == 0 && A[j] != 0 {
+            j = i
+        } else if A[i] != 0 && A[j] == 0 {
+            A.swapAt(i, j)
+            j += 1
+        } else if A[i] != 0 && A[j] != 0 {
+            j += 1
+        }
+    }
+    return A
+}
+
+moveAllZerosToEndOPTIMAL([1,0,0,3,0,0,4,1,1,1])
+
+
 
 func moveAllZerosToEndBRUTE(_ a: [Int]) -> [Int] {
     var result = [Int]()
+    var A = a
+    var zerosCount = 0
     
-    var j = 0
-    
-    for i in 0..<a.count {
-        if a[i] == 0 {
-            j = i
+    for i in A {
+        if i == 0 {
+            zerosCount += 1
+        } else {
+            result.append(i)
         }
-        
-        
+    }
+    
+    
+    for i in 1...zerosCount {
+        result.append(0)
+
     }
     
     return result
 }
+
+
+moveAllZerosToEndBRUTE([1 ,0 ,2 ,3 ,0 ,4 ,0 ,1])
+
+
