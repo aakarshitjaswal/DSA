b1e650fe6700533850b02b3f6d7a74d0a9e8aed9
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-11-30 18:33:36 +0530

    UPDATE: LengthOfTheLongestSubarrayWithZeroSum

--- Arrays/Arrays-Hard/LengthOfTheLongestSubarrayWithZeroSum-30-11-2023.playground/Contents.swift
+++ Arrays/Arrays-Hard/LengthOfTheLongestSubarrayWithZeroSum-30-11-2023.playground/Contents.swift
@@ -47,12 +47,37 @@ func longestZeroSumSubarrayBRUTE(_ a: [Int]) -> Int {
     return maxSoFar
 }
 
-var nums = [1, 3, -5, 6, -2]
+var nums = [6, -2, 2, -8, 1, 7, 4, -10]
 
 longestZeroSumSubarrayBRUTE(nums)
 
+func longestZeroSumSubarrayOPTIMAL(_ a: [Int]) -> Int {
+    
+    var hash:[Int:Int] = [:]
+    var maxSoFar = 0
+    var sum = 0
+    
+    for i in 0..<a.count {
+        sum += a[i]
+        
+        if sum == 0 {
+            maxSoFar = max(maxSoFar, i + 1)
+        } else if let prefixIndex = hash[sum] {
+            maxSoFar = max(maxSoFar, i - prefixIndex)
+        } else {
+            hash[sum] = i
+            print(hash)
 
+        }
+        
+    }
+    
+    return maxSoFar
+}
 
-
-
-
+var nums1 = [6, -2, 2, -8, 1, 7, 4, -10]
+var nums2 = [9, -3, 3, -1, 6, -5]
+var nums3 = [1, 0, -5]
+var nums4 = [1, 3, -5, 6, -2]
+// [6, -2, 2, -8, 1, 7, 4, -10]
+longestZeroSumSubarrayOPTIMAL(nums4)
