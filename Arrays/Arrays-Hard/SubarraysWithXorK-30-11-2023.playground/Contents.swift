9020be2211e69b37c09977fea233fca9aeb98069
aakarshitjaswal <aakarshitjaswal@gmail.com>
2023-12-01 17:06:15 +0530

    CREATE: SubarraysWithXorK

---
+++ Arrays/Arrays-Hard/SubarraysWithXorK-30-11-2023.playground/Contents.swift
@@ -0,0 +1,17 @@
+import Foundation
+
+/*
+ Example 1:
+ Input Format: A = [4, 2, 2, 6, 4] , k = 6
+ Result: 4
+ Explanation: The subarrays having XOR of their elements as 6 are  [4, 2], [4, 2, 2, 6, 4], [2, 2, 6], [6]
+
+ Example 2:
+ Input Format: A = [5, 6, 7, 8, 9], k = 5
+ Result: 2
+ Explanation: The subarrays having XOR of their elements as 5 are [5] and [5, 6, 7, 8, 9]
+ */
+
+func maxLengthXorKSubarraysBRUTE(_ a: [Int]) {
+    
+}
